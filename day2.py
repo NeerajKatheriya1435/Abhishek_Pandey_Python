3c42310e4224126c0ceceaf430be86e4d91e0384
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-30 11:08:27 +0530

    loops and strings and questions

---
+++ day2.py
@@ -0,0 +1,160 @@
+
+# print(4>5)
+# print(4>=5)
+# print(4<=5)
+# print(5<5)
+# print(4!=5)
+# print(4==5)
+
+# a=6
+
+# a=a+5
+# a+=5
+# a-=2
+# print(a)
+a=5
+b=3
+
+# print(a>b and a>6)
+# print(a>b or a<6)
+# print(a>b or a<6)
+
+# name="Abhishek Kumar how are you"
+# name="""Abhishek Kumar 
+# how are you
+# my name is neeraj"""
+# print(name)
+# print(name[2])
+# print(name[2:10])
+# print(name[2:])
+# print(name[:6])
+# print(name[::2])
+# print(name[-2])
+# print(name[-5:])
+# print(name)
+
+# name="       Rohan       "
+# print(name)
+# print(name.strip(" "))
+
+str1="Rohan Kumar Singh"
+# str2=str1.replace("Kumar","Singh")
+# print(str2)
+# print(str1)
+# print(str1.replace("Kumar","Singh"))
+# print(str1.split(" "))
+
+# str1 = "Welcome to the Console!!!"
+# print(str1.center(60))
+
+# str1 = "Welcome to the Console to !!!"
+# print(str1.endswith("!!!#"))
+
+# print(str1.find("to",10))
+# print(str1.index("to",10))
+
+# str1 = "Welcome ToTheConsole34656"
+# print(str1.isalnum())
+# str1 = "WelcomeToTheCons"
+# print(str1.isalpha())
+
+# str1 = "We wish \nyou a Merry Christmas"
+# print(str1.isprintable())
+
+# str1 = "world Health Organization"
+# print(str1.istitle())
+
+# str1 = "Python is a Interpreted Language"
+# print(str1.swapcase())
+
+# str1 = "python iS a interPreted Language"
+# print(str1.title())
+
+# str1="Hello"
+# print(str1[::-1])
+
+# for i in str1:
+#     str1.split()
+#     str1.reverse()
+# rev=""
+# for i in str1:
+#     rev=i+rev
+#     print(rev)
+# # print(rev)
+
+# str1="Rohan"
+# li=str1.split("")
+# li.reverse()
+# txt1=list(str1)
+# print(li)
+# txt1.reverse()
+# str1="".join(txt1)
+# print(str1)
+# for i in str1:
+# print(txt1)
+
+# str1="Rohan"
+
+# spt=list(str1)
+# spt.reverse()
+
+# str1="".join(spt)
+# print(spt)
+# print(str1)
+
+# age=int(input("Enter your age: "))
+
+# if(age<=0):
+#     print("Invalid age")
+# elif(age>0 and age<18):
+#     print("You are child")
+# elif(age>=18 and age<90):
+#     print("You can drive")
+# elif(age>=90 and age<130):
+#     print("You are overage")
+# else:
+#     print("Invalid age")
+
+# age=9
+
+# match(age):
+#     case 1|2|3|4:
+#         print("Sunday")
+    # case 34:
+    #     print("Monday")
+    # case 94:
+    #     print("Tuesday")
+    # case _:
+    #     print("Please Input 1 to 3")
+    # case _ if age%2==0:
+    #     print("Even Number")
+    # case _ if age%2==1:
+    #     print("Odd Number")
+
+
+# for i in range(5,11,2):
+#     print(i)
+
+# for i in range(100,1,-1):
+#     print(i)
+
+# table=454576
+# for i in range(1,11):
+#     print(i*table)
+
+# i=1
+# while(i<11):
+
+#     print(i)
+#     i+=1
+
+
+# for i in range(1,11):
+#     if(i==4):
+#         break
+#     print(i)
+
+# for i in range(1,11):
+#     if(i==4):
+#         continue
+#     print(i)
