ca5775b4df2a1a339bb3f62395d59011a2a612b8
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-30 09:56:25 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,60 @@
+
+# This is simple hello world
+
+"""
+hello i am python
+nice to meet you
+nice name
+djkscjfsd
+vknjvcfsd
+kjn
+sdcbj
+"""
+
+# print("Hello World!")
+# a=8
+# b=5
+# print(a+b)
+
+# print("Hello ' my \" name is Neeraj \nhow are you")
+# print("Hello\t\tare you")
+
+# a=6
+# b=7
+
+# abhishekkuma436456757r=67
+# print(abhishekkuma436456757r)
+
+# age=56
+# age=53
+# AGE=90
+# print(age)
+
+# a=True
+# print(a)
+# print(type(a))
+
+# a=7
+# b=5.6
+# c=int(a+b)
+# print(c)
+# print(type(c))
+
+# str1="Abhishek"
+# str2="Kumar"
+# str3="Singh"
+
+# a=8
+# b=7
+# print(str1,str2,str3,sep="-")
+# print(str1+str2+str3)
+# print(a+b)
+
+# print(str1,end=" ")
+# print(str2,end=" ")
+# print(str3)
+
+# inp1=int(input("Enter the num1: "))
+# inp2=int(input("Enter the num2: "))
+
+# print(inp1+inp2)
